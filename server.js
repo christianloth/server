7fb0e4f84b4a55d7c28fab39b3f8755502e02302
Christian Loth <christian@loth.me>
2022-03-10 12:47:48 -0600

    changes

--- server.js
+++ server.js
@@ -11,4 +11,4 @@ app.use('/login', (req, res) => {
     });
 });
 
-app.listen(8080, () => console.log('API is running on http://localhost:8080/login'));
+app.listen(80, () => console.log('API is running on http://localhost:80/login'));
